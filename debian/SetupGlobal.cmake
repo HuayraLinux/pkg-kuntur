3a0d60c03783d4bc54d9c529e9ef371f2edeaaa2
Fernando Toledo <ragnarok@docksud.com.ar>
2015-01-20 17:33:51 -0300

    Separando los paquetes por aplicacion, librerias y desarrollo

--- debian/SetupGlobal.cmake
+++ debian/SetupGlobal.cmake
@@ -1,5 +1,6 @@
-set (SILENT "0")
+set (SILENT "1")
 set (BUILD_SHARED "1")
 set (COMPILE_TESTS "0")
 set (RUN_TESTS "0")
-set (CMAKE_BUILD_TYPE "Debug")
+set (CMAKE_BUILD_TYPE "Release")
+set (CMAKE_CONFIG_INSTALL_PREFIX "/usr")
